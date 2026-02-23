5cd4f7dd790b36acd41032ee4bb70529ee81ccf4
FractalCap <fractalcapitalnetwork@gmail.com>
2026-02-23 04:34:03 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: '#004a99',
+        secondary: '#007EA7',
+        accent: '#003459',
+        light: '#F0F8FF',
+        steel: '#4b5563', // Steel Gray approximation
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
